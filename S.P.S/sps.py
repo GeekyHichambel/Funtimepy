f122518d377729e9e707f36bf2f936b9fb7b7579
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-12-30 21:38:52 +0530

    Update sps.py

--- S.P.S/sps.py
+++ S.P.S/sps.py
@@ -54,17 +54,30 @@ screen_width = 600
 screen_height = 600
 pygame.display.set_caption('S.P.S')
 screen = pygame.display.set_mode((screen_width,screen_height))
+
+#------------- RESOURCES ----------#
+
 bg = pygame.image.load("bg.png")
 stone = pygame.image.load("stone_butt.png")
 scissor = pygame.image.load("scissor_butt.png")
 paper = pygame.image.load("paper_butt.png")
+
+#-------------  MUSIC ----------#
+stone_sound = pygame.mixer.Sound('stone.mp3')
+paper_sound = pygame.mixer.Sound('paper.mp3')
+scissor_sound = pygame.mixer.Sound('scissor.mp3')
+stone_sound.set_volume(0.1)
+paper_sound.set_volume(0.1)
+scissor_sound.set_volume(0.1)
+
+#------------- DATA ----------#
 
 choices = ["Stone", "Paper", "Scissor"]
 player = ""
 computer_score = 0
 player_score = 0
 
-#-------------  BUTTONS ----------#
+#------------- BUTTONS ----------#
 
 stone_button = buttons(100,280,stone)
 scissor_button = buttons(240,280,scissor)
@@ -100,11 +113,14 @@ while working:
         print('The computer choses: '+computer)
         player = "Stone"
         if computer == player:
+            stone_sound.play()
             print('The rocks collide hence a tie')
         elif computer == "Scissor":
+            stone_sound.play()
             print('Hard hit of stone makes the scissor break hence u win')
             player_score+=1
         elif computer == "Paper":
+            paper_sound.play()
             print('Oops! The rock got caught by the paper hence u lose')
             computer_score+=1 
 
@@ -113,11 +129,14 @@ while working:
         print('The computer choses: '+computer)
         player = "Scissor"
         if computer == player:
+            scissor_sound.play()
             print('Oops! The scissors got stuck hence a tie')
         elif computer == "Stone":
+            stone_sound.play()
             print('Scissor got rammed by the stone hence u lose')
             computer_score+=1
         elif computer == "Paper":
+            scissor_sound.play()
             print('You cut the paper into pieces hence u win')
             player_score+=1
 
@@ -126,11 +145,14 @@ while working:
         print('The computer choses: '+computer)
         player = "Paper"
         if computer == player:
+            paper_sound.play()
             print('Two lame papers cannot do much hence a tie')
         elif computer == "Scissor":
+            scissor_sound.play()
             print('Paper got torn in halves hence u lose')
             computer_score+=1
         elif computer == "Stone":
+            paper_sound.play()
             print('You trapped the stone like a bait hence u win')
             player_score+=1
     
